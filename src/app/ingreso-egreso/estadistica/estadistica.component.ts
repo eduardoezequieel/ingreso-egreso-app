f45d94c5eb633917ca846708fd93f98f32ddf109
eduardoezequieel <riv.edu10@gmail.com>
2022-08-24 11:09:49 -0600

    First commit

---
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  templateUrl: './estadistica.component.html'
+})
+export class EstadisticaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
